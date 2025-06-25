8cbd64a55ce4f3b92e71ed44d37af8641f0b088b
Afewerki Kahsai <113409490+atk-25@users.noreply.github.com>
2025-06-24 22:38:08 -0700

    Update launch_evaluate_model.sh

--- launch_evaluate_model.sh
+++ launch_evaluate_model.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# "atk-25/Qwen2-VL-2B-Instruct-Audio-qlora"
-# --use_quantized_model \
-# --qlora_model_id "atk-25/Qwen2-VL-2B-Instruct-Audio-qlora" \
-
 python evaluate_model.py \
-    --base_model_id "atk-25/Qwen2-VL-2B-Instruct-Audio" \
-    --lora_adapters_id "atk-25/Qwen2-VL-2B-Instruct-Audio-LoRA-Adapters" \
+    --base_model_id "<base_model_id>" \
+    --lora_adapters_id "<lora_adapters_id>" \
     --use_quantized_model \
-    --qlora_model_id "atk-25/Qwen2-VL-2B-Instruct-Audio-qlora" \
+    --qlora_model_id "<qlora_model_id>" \
     --repos_private \
     --attn_implementation "flash_attention_2" \
     --per_device_eval_batch_size 2 \
