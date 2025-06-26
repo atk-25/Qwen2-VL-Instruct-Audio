2a929aa0994e891f8153da10a1c6615ca595f04a
Afewerki Kahsai <113409490+atk-25@users.noreply.github.com>
2025-06-25 23:46:06 -0700

    Update launch_evaluate_model.sh

--- launch_evaluate_model.sh
+++ launch_evaluate_model.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 python evaluate_model.py \
-    --base_model_id "<base_model_id>" \
-    --lora_adapters_id "<lora_adapters_id>" \
+    --base_model_id "<BASE_MODEL_ID>" \
+    --lora_adapters_id "<LORA_ADAPTERS_ID>" \
     --use_quantized_model \
-    --qlora_model_id "<qlora_model_id>" \
+    --qlora_model_id "<QLORA_MODEL_ID>" \
     --repos_private \
     --attn_implementation "flash_attention_2" \
-    --per_device_eval_batch_size 2 \
-    --number_samples_for_eval 32 \
+    --per_device_eval_batch_size 8 \
+    --number_samples_for_eval 128 \
     --max_new_tokens 256
